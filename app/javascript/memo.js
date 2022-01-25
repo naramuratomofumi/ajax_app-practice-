be6f1a7e188073328c939ef044cd553e564b3b3c
naramuratomofumi <tomofuminaramura@gmail.com>
2022-01-25 19:43:07 +0900

    パスの修正

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -4,7 +4,7 @@ function post (){
     const form = document.getElementById("form");
     const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
-    XHR.open("post", posts_path, true);
+    XHR.open("post", "/posts", true);
     XHR.responseType = "json";
     XHR.send(formData);
   });
