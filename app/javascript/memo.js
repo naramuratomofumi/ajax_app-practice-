9397a3c2580bc17e1d57bb9e61c0dce5a9d16253
naramuratomofumi <tomofuminaramura@gmail.com>
2022-01-25 19:28:55 +0900

    FormDataオブジェクトを使って、フォームの値を取得するように設定

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -2,6 +2,7 @@ function post (){
   const submit = document.getElementById("submit");
   submit.addEventListener("click", () => {
     const form = document.getElementById("form");
+    const formData = new FormData(form);
   });
 };
 
